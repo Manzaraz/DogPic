dd8618669811a0eff7a074db14757e430dc7c8a3
Christian Manzaraz <magnusmanz@gmail.com>
2024-01-10 16:49:34 -0300

    Initial Commit

---
+++ DogPic/DogPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPicApp.swift
+//  DogPic
+//
+//  Created by Christian Manzaraz on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DogPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
